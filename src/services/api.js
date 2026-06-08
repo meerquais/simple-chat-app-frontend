62df4cd4650ed77f011f54bddd2905d62c60c00c
Meer Quais <meerquais017@gmail.com>
2026-06-08 17:36:50 +0500

    updated url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:"simple-chat-app-backend-production.up.railway.app/api"
 });
 
 export default api;
